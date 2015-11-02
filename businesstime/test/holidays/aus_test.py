425c2551a0d59320c6a5bcbcabe466ff5f5732a8
Eric Waller <erwaller@gmail.com>
2015-11-02 14:39:52 -0500

    Addt'l aus test

--- businesstime/test/holidays/aus_test.py
+++ businesstime/test/holidays/aus_test.py
@@ -9,7 +9,7 @@ class QueenslandPublicHolidaysTest(unittest.TestCase):
     def test_2016_08(self):
         holidays_gen = QueenslandPublicHolidays()
         self.assertEqual(
-            list(holidays_gen(date(2016, 8, 1), end=date(2016, 9, 1))),
+            list(holidays_gen(date(2016, 8, 1), end=date(2016, 8, 31))),
             []
         )
 
@@ -19,8 +19,13 @@ class BrisbanePublicHolidaysTest(unittest.TestCase):
     def test_2016_08(self):
         holidays_gen = BrisbanePublicHolidays()
         self.assertEqual(
-            list(holidays_gen(date(2016, 8, 1), end=date(2016, 9, 1))),
+            list(holidays_gen(date(2016, 8, 1), end=date(2016, 8, 31))),
             [
                 date(2016, 8, 10)
             ]
         )
+
+    def test_out_of_range(self):
+        holidays_gen = BrisbanePublicHolidays()
+        with self.assertRaises(NotImplementedError):
+            list(holidays_gen(date(2017, 1, 1), end=date(2017, 12, 31)))
